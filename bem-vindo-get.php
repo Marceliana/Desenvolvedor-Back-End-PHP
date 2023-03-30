f92a802f2ce48d91b50e8f6e0df273634c3d5dd0
Marceliana <89810151+Marceliana@users.noreply.github.com>
2023-03-29 23:49:17 -0300

    Add files via upload
    Exercício 06 - Interação entre FROM e PHP- Bem-vindo-get. Bem-vindo-Post, index.html

---
+++ bem-vindo-get.php
@@ -0,0 +1,7 @@
+< h1 > Formulario usa metodo GET </ h1 >
+    < form  action =" bem-vindo-get.php " method =" get " >
+        Nome: < input  type =" text " name =" nome " > < br >
+        E-mail: < input  type =" text " name =" email " > < br >
+        Idade:< input type = "text "idade = "idade" > < br >
+        < tipo de entrada  =" enviar " >
+    </forma> _ _
