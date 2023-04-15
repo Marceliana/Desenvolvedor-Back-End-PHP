7d78889f07a94a100909d09d5bee83bf0bb2a0b4
Marceliana <89810151+Marceliana@users.noreply.github.com>
2023-04-15 09:52:54 -0300

    Add files via upload
    Exercício 10 -   https://br.000webhost.com/members/website/marceliana/build

---
+++ cadastrar-aluno.php
@@ -0,0 +1,96 @@
+!<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lista de alunos</title>
+</head>
+<body>
+        <form action="cadastrar-aluno.php" method="POST">
+        Nome Completo: <br>
+        <input type="text" name="nome" required><br>
+        Link GitHub: <br>
+        <input type="text" name="link-github" required><br>
+        Link WebHost: <br>
+        <input type="text" name="link-webhost" required><br>
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
+    <table border=1>
+        <tr>
+            <th>Nome</th>
+            <th>Link GitHub</th>
+            <th>Link WebHost</th>
+        </tr>
+        
+                <tr>
+                    <td>LUIZ FELIPE FREIRE MAIA</td>
+                    <td><a target='_blank' href='https://github.com/luiz87'>https://github.com/luiz87</a></td>
+                    <td><a target='_blank'  href='https://luiz-senai.000webhostapp.com/'>https://luiz-senai.000webhostapp.com/</a></td>
+                </tr>
+                
+                <tr>
+                    <td>Stefane L.</td>
+                    <td><a target='_blank' href='https://github.com/stefanelorranne'>https://github.com/stefanelorranne</a></td>
+                    <td><a target='_blank'  href='＼（〇_ｏ）／'>＼（〇_ｏ）／</a></td>
+                </tr>
+                
+                <tr>
+                    <td>Wellington Rodrigues da Silva</td>
+                    <td><a target='_blank' href='https://github.com/wrsilva832/cursophp'>https://github.com/wrsilva832/cursophp</a></td>
+                    <td><a target='_blank'  href='https://wellington-senai.000webhostapp.com/'>https://wellington-senai.000webhostapp.com/</a></td>
+                </tr>
+                
+                <tr>
+                    <td>Davi Sales Barcelos</td>
+                    <td><a target='_blank' href='https://github.com/SallesThe'>https://github.com/SallesThe</a></td>
+                    <td><a target='_blank'  href='https://sallesthe.000webhostapp.com/'>https://sallesthe.000webhostapp.com/</a></td>
+                </tr>
+                
+                <tr>
+                    <td>Wellington Rodrigues da Silva</td>
+                    <td><a target='_blank' href='https://github.com/wrsilva832/cursophp'>https://github.com/wrsilva832/cursophp</a></td>
+                    <td><a target='_blank'  href='https://wellington-senai.000webhostapp.com/'>https://wellington-senai.000webhostapp.com/</a></td>
+                </tr>
+                
+                <tr>
+                    <td>Wellington Rodrigues da Silva</td>
+                    <td><a target='_blank' href='https://github.com/wrsilva832/cursophp'>https://github.com/wrsilva832/cursophp</a></td>
+                    <td><a target='_blank'  href='https://wellington-senai.000webhostapp.com/'>https://wellington-senai.000webhostapp.com/</a></td>
+                </tr>
+                
+                <tr>
+                    <td>edmarcos de  araujo pacheco</td>
+                    <td><a target='_blank' href='https://github.com/edmarcos12'>https://github.com/edmarcos12</a></td>
+                    <td><a target='_blank'  href='~~'>~~</a></td>
+                </tr>
+                
+                <tr>
+                    <td>Stefane L.</td>
+                    <td><a target='_blank' href='https://github.com/stefanelorranne'>https://github.com/stefanelorranne</a></td>
+                    <td><a target='_blank'  href='＼（〇_ｏ）／'>＼（〇_ｏ）／</a></td>
+                </tr>
+                
+                <tr>
+                    <td>Kelizan Santos Leite Vidal</td>
+                    <td><a target='_blank' href='https://github.com/kelizan'>https://github.com/kelizan</a></td>
+                    <td><a target='_blank'  href='https://kelizan-projeto1.000webhostapp.com'>https://kelizan-projeto1.000webhostapp.com</a></td>
+                </tr>
+                
+                <tr>
+                    <td>edmarcos de araujo pacheco</td>
+                    <td><a target='_blank' href='	https://github.com/edmarcos12'>	https://github.com/edmarcos12</a></td>
+                    <td><a target='_blank'  href='https://edmarcosphp.000webhostapp.com/'>https://edmarcosphp.000webhostapp.com/</a></td>
+                </tr>
+                
+                <tr>
+                    <td>Isaque Kirmse Mendonça Soares</td>
+                    <td><a target='_blank' href='https://github.com/Kirmse'>https://github.com/Kirmse</a></td>
+                    <td><a target='_blank'  href='https://isaque-kirmse.000webhostapp.com'>https://isaque-kirmse.000webhostapp.com</a></td>
+                </tr>
+                    </table>
+    <script>
+        tgimg = document.getElementsByTagName('img');
+        tgimg[tgimg.length-1].style = 'display: none';
+    </script>
+<div style="text-align:right;position:fixed;z-index:9999999;bottom:0;width:auto;right:1%;cursor:pointer;line-height:0;display:block!important"><a title="Hosted on free web hosting 000webhost.com. Host your own website for FREE." target="_blank" href="https://www.000webhost.com/?utm_source=000webhostapp&utm_campaign=000_logo&utm_medium=website&utm_content=footer_img"><img src="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png" alt="www.000webhost.com"></a></div><script>function getCookie(t){for(var e=t+"=",n=decodeURIComponent(document.cookie).split(";"),o=0;o<n.length;o++){for(var i=n[o];" "==i.charAt(0);)i=i.substring(1);if(0==i.indexOf(e))return i.substring(e.length,i.length)}return""}getCookie("hostinger")&&(document.cookie="hostinger=;expires=Thu, 01 Jan 1970 00:00:01 GMT;",location.reload());var wordpressAdminBody=document.getElementsByClassName("wp-admin")[0],notification=document.getElementsByClassName("notice notice-success is-dismissible"),hostingerLogo=document.getElementsByClassName("hlogo"),mainContent=document.getElementsByClassName("notice_content")[0];if(null!=wordpressAdminBody&&0<notification.length&&null!=mainContent){var googleFont=document.createElement("link");googleFontHref=document.createAttribute("href"),googleFontRel=document.createAttribute("rel"),googleFontHref.value="https://fonts.googleapis.com/css?family=Roboto:300,400,600,700",googleFontRel.value="stylesheet",googleFont.setAttributeNode(googleFontHref),googleFont.setAttributeNode(googleFontRel);var css="@media only screen and (max-width: 576px) {#main_content {max-width: 320px !important;} #main_content h1 {font-size: 30px !important;} #main_content h2 {font-size: 40px !important; margin: 20px 0 !important;} #main_content p {font-size: 14px !important;} #main_content .content-wrapper {text-align: center !important;}} @media only screen and (max-width: 781px) {#main_content {margin: auto; justify-content: center; max-width: 445px;}} @media only screen and (max-width: 1325px) {.web-hosting-90-off-image-wrapper {position: absolute; max-width: 95% !important;} .notice_content {justify-content: center;} .web-hosting-90-off-image {opacity: 1;}} @media only screen and (min-width: 769px) {.notice_content {justify-content: space-between;} #main_content {margin-left: 5%; max-width: 445px;} .web-hosting-90-off-image-wrapper {position: absolute; display: flex; justify-content: center; width: 50%; margin-left: 45%;}} .web-hosting-90-off-image {max-width: 90%;} .content-wrapper {min-height: 454px; display: flex; flex-direction: column; justify-content: center; z-index: 5} .notice_content {display: flex; align-items: center;} * {-webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale;} .upgrade_button_red_sale{box-shadow: 0 2px 4px 0 rgba(255, 69, 70, 0.2); width: 264px; border: 0; border-radius: 3px; background-color: #FF5C62 !important; padding: 15px 55px !important; font-family: 'Roboto', sans-serif; font-size: 16px; font-weight: 600; color: #ffffff;} .upgrade_button_red_sale:hover{color: #ffffff !important; background: #d10303 !important;}",style=document.createElement("style"),sheet=window.document.styleSheets[0];style.styleSheet?style.styleSheet.cssText=css:style.appendChild(document.createTextNode(css)),document.getElementsByTagName("head")[0].appendChild(style),document.getElementsByTagName("head")[0].appendChild(googleFont);var button=document.getElementsByClassName("upgrade_button_red")[0],link=button.parentElement;link.setAttribute("href","https://www.hostinger.com/hosting-starter-offer?utm_source=000webhost&utm_medium=panel&utm_campaign=000-wp"),link.innerHTML='<button class="upgrade_button_red_sale">Claim Deal</button>',(notification=notification[0]).setAttribute("style","padding-bottom: 0; padding-top: 5px; background-color: #040713; background-size: cover; background-repeat: no-repeat; color: #ffffff; border-left-color: #040713;"),notification.className="notice notice-error is-dismissible";var mainContentHolder=document.getElementById("main_content");mainContentHolder.setAttribute("style","padding: 0;"),hostingerLogo[0].remove();var h1Tag=notification.getElementsByTagName("H1")[0];h1Tag.className="000-h1",h1Tag.innerHTML="Black Friday",h1Tag.setAttribute("style",'color: white; font-family: "Roboto", sans-serif; font-size: 48px; font-weight: 700;');var h2Tag=document.createElement("H2");h2Tag.innerHTML="Up to 90% off 4-year hosting plans + free domain, SSL & DDoS protection",h2Tag.setAttribute("style",'color: white; margin: 10px 0 15px 0; font-family: "Roboto", sans-serif; font-size: 16px; font-weight: 400; line-height: 1;'),h1Tag.parentNode.insertBefore(h2Tag,h1Tag.nextSibling);var paragraph=notification.getElementsByTagName("p")[0];paragraph.innerHTML="$<span style='font-size: 80px;'>2.49</span>/mo",paragraph.setAttribute("style",'font-family: "Roboto", sans-serif; font-size: 48px; font-weight: 700; margin: 0;');var list=notification.getElementsByTagName("UL")[0];list.remove();var org_html=mainContent.innerHTML,new_html='<div class="content-wrapper">'+mainContent.innerHTML+'</div><div class="web-hosting-90-off-image-wrapper"><img class="web-hosting-90-off-image" src="https://cdn.000webhost.com/000webhost/promotions/bf-2022-bottom-banner.png"></div>';mainContent.innerHTML=new_html;var saleImage=mainContent.getElementsByClassName("web-hosting-90-off-image")[0]}</script></body>
+</html>
--- index.php
+++ index.php
@@ -1,7 +1,12 @@
-<?php
-echo "Olá Mundo";
-?>
- < br >
-    < a  href =" exemplos-php/interacao-form-php/index.html " > Exemplo Interação Form PHP </ a >
-</ corpo >
-</html> _ _
+<DOCTYPE html>
+    <html lang ="en"
+    <head>
+        <meta charset="UTF-8">
+       <title>Minha página no WebHost</title>
+        </head>
+        <body>
+        <?php
+        echo "By Marceliana Maria da Silva.";
+        ?>
+         </body>
+         </html>
